673af2106f2ab149f202e650f3ab9f94498daee0
manorajesh <mano@raje.sh>
2023-03-05 16:55:40 -0800

    Complete overall of the logic while maintaining similar UI

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,78 @@
-use std::{process::Command, thread};
+use std::process::Command;
+use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let ip_range: String = "192.168.32.*".to_owned();
-    let num_of_threads: i32 = 10;
-    let mut start: i32 = 0;
-    let mut threads: Vec<_> = Vec::new();
-    
-    for _ in 0..num_of_threads {
-        let end = start + 255/num_of_threads;
-        let ip_range = ip_range.clone();
-        let t = thread::spawn(move || {
-            ping(ip_range, start, end);
-        });
-        start = end;
-        threads.push(t);
-    }
+    let ip_range = String::from("192.168.32.*");
+    let output = Arc::new(Mutex::new(Vec::new()));
+    let total_ips = 255;
 
-    for t in threads {
-        t.join().unwrap();
+    (0..255).into_par_iter().for_each(|i| {
+        let ping_lines = ping(ip_range.replace("*", &i.to_string()));
+        let mut blocker = output.lock().unwrap();
+        blocker.push(ping_lines);
+        progress_bar(blocker.len(), total_ips);
+    });
+
+    output.lock().unwrap().sort_by(|a, _| {
+        if a.contains("is up") {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Less
+        }
+    });
+
+    println!();
+    for line in output.lock().unwrap().iter() {
+        println!("{}", line);
     }
 }
 
-fn ping(ip_range: String, start: i32, end: i32) {
-    for i in start..end {
-        let ip = ip_range.replace("*", &format!("{}", i).as_str());
-        let output = if cfg!(target_os = "windows") {
-            Command::new("ping")
-                .args(&["/n", "1", &ip])
-                .output()
-                .expect("failed to execute process")            
+fn ping(ip: String) -> String {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("ping")
+            .args(&["/n", "1", &ip])
+            .output()
+            .expect("failed to execute process")            
+    } else {
+        Command::new("ping")
+            .arg("-c")
+            .arg("1")
+            .arg(&ip)
+            .output()
+            .expect("failed to execute process")
+    };
+    
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    if stdout.contains("TTL") {
+        let ip = format!("{0:15}", ip);
+        if let Some(time) = stdout.split("time").nth(1) {
+            let time = time.split("ms").next().unwrap().replace("=", "");
+            let line = format!("{ip}is up \x1b[5;32m•\x1b[0m \x1b[38;5;8m({time}ms)\x1b[0m", time = time);
+            return line;
         } else {
-            Command::new("ping")
-                .arg("-c")
-                .arg("1")
-                .arg(&ip)
-                .output()
-                .expect("failed to execute process")
-        };
-        
-        let text = format!("{:?}", output.stdout);
-        if text.contains("time") && output.status.success() {
-            println!("{}is up \x1b[5;32m•\x1b[0m", format!("{0:15}", ip));
-        } else {
-            println!("{}is down \x1b[5;31m•\x1b[0m", format!("{0:15}", ip));
+            let line = format!("{ip}is up \x1b[5;32m•\x1b[0m \x1b[37m(unknown ms)\x1b[0m"); // two spaces to line up
+            return line;
         }
+    } else {
+        let ip = format!("{0:15}", ip);
+        let line = format!("{ip}is down \x1b[5;31m•\x1b[0m");
+        return line;
     }
+}
+
+fn progress_bar(current: usize, total: usize) {
+    let mut progress = String::new();
+    let mut percent = current as f32 / total as f32 * 100.0;
+    percent = percent.round();
+    let mut i = 0;
+    while i < percent as usize {
+        progress.push_str("=");
+        i += 1;
+    }
+    while i < 100 {
+        progress.push_str(" ");
+        i += 1;
+    }
+    print!("\r[{}] {}%", progress, percent);
 }
