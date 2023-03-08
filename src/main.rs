c44c68177231488d4a810e01d7d3291e8949c539
manorajesh <mano@raje.sh>
2023-03-08 13:18:59 -0800

    Added POSIX support for ping and added CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use std::process::Command;
 use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
+use std::{process::{Command, exit}, sync::{Arc, Mutex}};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    version = "0.0.2",
+    author = "Mano Rajesh",
+    about = "Scan the local network for active hosts."
+)]
+
+struct Args {
+    /// IP range to scan, use wildcard "*" to define range [default: 192.168.1.*]
+    ip_range: String,
+
+    /// How to wait for a response [default: 1 second]
+    #[arg(short = 't', long = "timeout", value_name="SECONDS")]
+    timeout: Option<usize>,
+
+    /// Total IPs to scan [default: 255]
+    #[arg(short = 'n', long = "total", value_name="TOTAL")]
+    total: Option<usize>,
+}
 
 fn main() {
-    let ip_range = String::from("192.168.32.*");
-    let output = Arc::new(Mutex::new(Vec::new()));
-    let total_ips = 255;
+    let args = Args::parse();
+    if !args.ip_range.contains("*") {
+        println!("Invalid IP range");
+        exit(1);
+    }
 
-    (0..255).into_par_iter().for_each(|i| {
-        let ping_lines = ping(ip_range.replace("*", &i.to_string()));
+    let ip_range = String::from(args.ip_range);
+    let output = Arc::new(Mutex::new(Vec::new()));
+    let total_ips = args.total.unwrap_or(255);
+
+    (0..total_ips).into_par_iter().for_each(|i| {
+        let ping_lines = ping(ip_range.replace("*", &i.to_string()), args.timeout.unwrap_or(1));
         let mut blocker = output.lock().unwrap();
         blocker.push(ping_lines);
         progress_bar(blocker.len(), total_ips);
@@ -28,27 +54,32 @@ fn main() {
     }
 }
 
-fn ping(ip: String) -> String {
+fn ping(ip: String, timeout: usize) -> String {
     let output = if cfg!(target_os = "windows") {
         Command::new("ping")
             .args(&["/n", "1", &ip])
+            .arg(&format!("-w{}", timeout))
             .output()
-            .expect("failed to execute process")            
+            .expect("failed to execute process")
     } else {
         Command::new("ping")
             .arg("-c")
             .arg("1")
             .arg(&ip)
+            .arg(&format!("-t{}", timeout))
             .output()
             .expect("failed to execute process")
     };
-    
+
     let stdout = String::from_utf8_lossy(&output.stdout);
-    if stdout.contains("TTL") {
+    if stdout.contains("TTL") || stdout.contains("ttl") {
         let ip = format!("{0:15}", ip);
         if let Some(time) = stdout.split("time").nth(1) {
             let time = time.split("ms").next().unwrap().replace("=", "");
-            let line = format!("{ip}is up \x1b[5;32m•\x1b[0m \x1b[38;5;8m({time}ms)\x1b[0m", time = time);
+            let line = format!(
+                "{ip}is up \x1b[5;32m•\x1b[0m \x1b[38;5;8m({time}ms)\x1b[0m",
+                time = time
+            );
             return line;
         } else {
             let line = format!("{ip}is up \x1b[5;32m•\x1b[0m \x1b[37m(unknown ms)\x1b[0m"); // two spaces to line up
@@ -70,6 +101,7 @@ fn progress_bar(current: usize, total: usize) {
         progress.push_str("=");
         i += 1;
     }
+    progress.push_str(">");
     while i < 100 {
         progress.push_str(" ");
         i += 1;
